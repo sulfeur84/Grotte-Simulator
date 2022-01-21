088e83dbf513d0734ceb09bc8b462776c420de8e
PLANCQ Valentin <sulfeur84@gmail.com>
2022-01-21 11:31:17 +0100

    triangulation

---
+++ Assets/Triangulation/TriangulaSpawn.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class TriangulaSpawn : MonoBehaviour
+{
+    public GameObject Salle;
+    public int MaxSalle;
+    private Transform MyTrans;
+    public float MinX, MaxX, MinY, MaxY;
+
+    public void Awake()
+    {
+        MyTrans = GetComponent<Transform>();
+    }
+    void Start()
+    {
+        for (int i = 0; i < MaxSalle; i++)
+        {
+            Instantiate(Salle, new Vector3(MyTrans.position.x + Random.Range(MinX, MaxX), 
+                MyTrans.position.y + Random.Range(MinY, MaxY)), Quaternion.identity);;
+        }
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R)) SceneManager.LoadScene("Triangulation");    
+    }
+}
